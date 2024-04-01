1b6a94be95c11deb4f6f532b5851f10dcd4fae81
KU-5H <kushpatel76@yahoo.com>
2024-03-31 20:30:56 -0400

    Made annoucenment schemea

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useState, useContext} from "react";
 import {UserContext} from '../../context/userContext'
 import { Navigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
+import axios from "axios";
 import { v4 as uuidv4 } from 'uuid';
 
 // Home page should be for announcements/updates
@@ -16,6 +17,11 @@ function Home() {
     { title: 'Title 3', body: 'here is some text about the announcement' }
   ]);
 
+  const [data, setData] = useState({
+    title: '',
+    text: '',
+  })
+
   const [newAnnouncement, setNewAnnouncement] = useState([])
 
   function createAnnouncementDraft() {
@@ -28,25 +34,28 @@ function Home() {
 
   function updateAnnouncements(e) {
     e.preventDefault();
+    console.log(data)
     const titleA = e.target[0].value;
     const bodyA = e.target[1].value;
     // send api call to database to update announcements
     cancelAnnouncement();
     setAnnouncements([{title: titleA, body: bodyA}, ...announcements]);
   }
+
   
+
 
   return (
     <div>
-      {!!user ? (<div className="announcements-page">
+      {user ? (<div className="announcements-page">
         <div className="announcements-title">
           <p className="pages-title">Announcements</p>
           {user.role === 'Admin' && (<p className='add-announcement' onClick={() => createAnnouncementDraft()}>+</p>)}
         </div>
         {newAnnouncement.map(announcement => (
-          <form method="post" onSubmit={(e) => updateAnnouncements(e)} key={uuidv4()} className="announcement-container">
-            <input type="text" />
-            <textarea className="new-announcement-textarea"/>
+          <form onSubmit={updateAnnouncements} key={uuidv4()} className="announcement-container">
+            <input type="text" placeholder="Title Text..."  value={data.title} onChange={(e) => setData({...data, title: e.target.value})}/>
+            <textarea placeholder="text goes here..." value={data.text} className="new-announcement-textarea" onChange={(e) => setData({...data, text: e.target.value})} />
             <div className="announcement-options">
               <button className="announcement-option cancel">Cancel</button>
               <button type="submit" className="announcement-option submit">Submit</button>
