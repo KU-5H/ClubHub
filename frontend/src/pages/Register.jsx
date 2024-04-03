bc4f0ce917a9e5c95bacbe1d9e43a04f53d51ce9
Lex Pan <lexiang.pan@gmail.com>
2024-04-03 14:37:41 -0400

    added register button styling

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React from 'react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { toast } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
@@ -12,6 +12,10 @@ export default function Register() {
         password: '',
         role: '',
     })
+
+    const refOne = useRef(null);
+    const refTwo = useRef(null);
+    const refThree = useRef(null);
 
     const handleRegister = async (e) => {
         e.preventDefault();
@@ -41,7 +45,32 @@ export default function Register() {
         )
     }
 
-
+    function setRole(num) {
+        if (num == 1) {
+            const btn1 = refOne.current;
+            btn1.className = "role-button selected";
+            refTwo.current.className = "role-button";
+            refThree.current.className = "role-button";
+            setData({...data, role: "Member"}); 
+            toastNoti("Member");            
+        }
+        if (num == 2) {
+            const btn2 = refTwo.current;
+            btn2.className = "role-button selected";
+            refOne.current.className = "role-button";
+            refThree.current.className = "role-button";
+            setData({...data, role: "Treasurer"}); 
+            toastNoti("Treasurer");
+        }
+        if (num == 3) {
+            const btn3 = refThree.current;
+            btn3.className = "role-button selected";
+            refOne.current.className = "role-button";
+            refTwo.current.className = "role-button";
+            setData({...data, role: "Admin"}); 
+            toastNoti("Admin");
+        }
+    }
 
     return (
         <div className='register-page'>
@@ -64,9 +93,9 @@ export default function Register() {
             </div>
             <h1>Pick a Role</h1>
             <div className='register-buttons'>
-                <button type='button' className='role-button' onClick={function(e){setData({...data, role: "Member"}); toastNoti("Member")}}>Member</button>
-                <button type='button' className='role-button' onClick={function(e){setData({...data, role: "Treasurer"}); toastNoti("Treasurer")}}>Treasurer</button>
-                <button type='button' className='role-button' onClick={function(e){setData({...data, role: "Admin"}); toastNoti("Admin")}}>Admin</button>
+                <button type='button' ref={refOne} className='role-button' onClick={() => setRole(1)}>Member</button>
+                <button type='button' ref={refTwo} className='role-button' onClick={() => setRole(2)}>Treasurer</button>
+                <button type='button' ref={refThree} className='role-button' onClick={() => setRole(3)}>Admin</button>
             </div>
             <button type='submit' className='register-button'>Register</button>  
             </form>
